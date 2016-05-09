e598a218981ba32b2092f2f5af3ebe39520b61b5
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-05-08 20:07:49 -0700

    fixed skills controller

--- js/skills/skills.state.js
+++ js/skills/skills.state.js
@@ -2,6 +2,6 @@ app.config(function($stateProvider){
   $stateProvider.state('skills', {
     url: '/skills',
     templateUrl: '/js/skills/skills.html',
-    controller: '/js/skills/skills.controller.js'
+    controller: 'Skills'
   })
 })
